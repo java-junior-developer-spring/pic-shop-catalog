58dba0c04f185a10f67db2bdb85d1eb38f5e344e
dasha <jjg-lessons@gmail.com>
2024-03-06 00:30:24 +0300

    Создание, настройки

---
+++ src/main/java/com/picshop/catalog/PicShopCatalogApplication.java
@@ -0,0 +1,13 @@
+package com.picshop.catalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicShopCatalogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PicShopCatalogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/picshop/catalog/PicShopCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.picshop.catalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicShopCatalogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
